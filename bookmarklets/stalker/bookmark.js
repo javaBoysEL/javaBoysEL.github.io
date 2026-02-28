687c51d93a0f6fe55139b59e811d7b0c177214dc
Java Boys <ianv6207@mytusd.org>
2026-02-27 16:27:53 -0800

    Create bookmark.js

---
+++ bookmarklets/stalker/bookmark.js
@@ -0,0 +1,3 @@
+document.getElementById("bookmark").href = `
+javascript:const doge = document.createElement("img"); doge.src = "https://raw.githubusercontent.com/javaBoysEL/assets/refs/heads/main/evilDoge.jpeg"; doge.style.position = "fixed"; doge.style.bottom = "0px"; doge.style.width = "100%"; doge.style.height = "45%"; doge.style.zIndex = 9999999999999; document.body.appendChild(doge);
+`;
