50fe28bf60a5dd8eb5e34ddc5efd9422535a9295
Java Boys <ianv6207@mytusd.org>
2026-03-06 14:36:06 -0800

    Delete bookmarklets directory

--- bookmarklets/67 Attack/bookmark.js
+++
@@ -1,4 +0,0 @@
-const hyperLink = document.getElementById("bookmark");
-hyperLink.href = `
-javascript:const%20Kids%20=%20document.getElementsByTagName(%22img%22);%20for%20(let%20Master%20of%20Kids)%20%7BMaster.src%20=%20%22https%3A//townlift.com/wp-content/uploads/2025/10/Untitled-Graph-16-1024x675.jpeg%22;%7D
-`;
